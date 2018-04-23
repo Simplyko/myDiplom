edd291bfc4546c4c6b0d9c3e3b431071b61e490a
MsSon1Q <elizabethnovikova07@gmail.com>
2018-04-23 16:13:09 +0300

    frontend changes

--- app/overrides/auth_shared_login_bar.rb
+++ app/overrides/auth_shared_login_bar.rb
@@ -2,5 +2,5 @@ Deface::Override.new(virtual_path: "spree/shared/_nav_bar",
                      name: "auth_shared_login_bar",
                      insert_before: "li#search-bar",
                      partial: "spree/shared/login_bar",
-                     disabled: false,
+                     disabled: true,
                      original: 'c00d67dfc0496d21a351b48c41a728412cb5ed2c')
