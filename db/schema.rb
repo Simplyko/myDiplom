4b7a90d71faef1301862007ca7771685f4a486ad
simplyko <simplyko@mail.ru>
2018-04-27 07:47:55 +0300

    recreate db

--- db/schema.rb
+++ db/schema.rb
@@ -1277,7 +1277,6 @@ ActiveRecord::Schema.define(version: 20180420103524) do
     t.datetime "updated_at", null: false
     t.datetime "discontinue_on"
     t.datetime "created_at", null: false
-    t.decimal "sale_price", precision: 8, scale: 2
     t.index ["deleted_at"], name: "index_spree_variants_on_deleted_at"
     t.index ["discontinue_on"], name: "index_spree_variants_on_discontinue_on"
     t.index ["is_master"], name: "index_spree_variants_on_is_master"
